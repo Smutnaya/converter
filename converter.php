1201a9829385ef9bd090850bc1b4ad41d4765152
Smutnaya <lana900033@gmail.com>
2021-06-13 21:03:44 +0300

    api

--- converter.php
+++ converter.php
@@ -19,60 +19,39 @@
 		
 		<?php
 
-		  //$string = file_get_contents("https://www.nbrb.by/api/exrates/rates/145");
-		//echo($string); 
-		
+		$j_kurs = 'https://www.nbrb.by/api/exrates/rates?periodicity=0';
+		$json = json_decode(file_get_contents($j_kurs));
+				
 		if ($_GET["go"]) {
-			$date_today = date("m/d/Y"); // вычисляем сегодняшнюю дату
-			$xml_kurs = 'http://www.nbrb.by/Services/XmlExRates.aspx?ondate='.$date_today;
-			$xml = simplexml_load_file($xml_kurs);
+
 			if($_GET["cash"] != null)
 			{
-				if($_GET["to"]==0){
-					foreach ($xml->Currency as $Currency) {
-						switch((string) $Currency['Id']) { 
-					case '145': //USD
-						$usd = $Currency->Rate;
-						break;
-						}
-					}
-					
-					$result = $_GET["cash"] / $usd;	
-					echo($_GET["cash"].' BYN = '.round($result,2).' USD');
-				}
-				
-				if($_GET["to"]==1){
-					foreach ($xml->Currency as $Currency) {
-					switch((string) $Currency['Id']) { 
-
-					case '292':  //EUR
-						$eur = $Currency->Rate;
-						break;
-						}
-					}
-					
-					$result = $_GET["cash"] / $eur;	
-					echo($_GET["cash"].' BYN = '.round($result,2).' EUR');
-				}
-				
-				if($_GET["to"]==2){
-					foreach ($xml->Currency as $Currency) {
-					switch((string) $Currency['Id']) { 
-					case '298':  //EUR
-						$rub = $Currency->Rate;
-						break;
-						}
-					}
-					$result = (100/ $rub) * $_GET["cash"];	
-					$res = $_GET["cash"].' BYN = '.round($result,0).' RUB';
+				if($_GET["to"]==0){					
+					$kurs = ($json[4]->Cur_OfficialRate);					
+					$result = $_GET["cash"] / $kurs;	
+					$res = ($_GET["cash"].' BYN = '.round($result,2).' USD');
 					echo($res);
 					setcookie("conv",$res);
 				}
 				
-				echo('<br><a href="pdf.php">PDF</a>');
+				if($_GET["to"]==1){ //EUR
+					$kurs = ($json[5]->Cur_OfficialRate);					
+					$result = $_GET["cash"] / $kurs;	
+					$res = ($_GET["cash"].' BYN = '.round($result,2).' EUR');
+					echo($res);
+					setcookie("conv",$res);
 				}
-
-			}			
+				
+				if($_GET["to"]==2){					
+					$kurs = ($json[16]->Cur_OfficialRate);
+					$result = (100/ $kurs) * $_GET["cash"];	
+					$res = $_GET["cash"].' BYN = '.round($result,0).' RUB';
+					echo($res);
+					setcookie("conv",$res);
+				}
+				echo('<br><a href="pdf.php">PDF</a>');	
+			}
+		}				
 		
 		?> 
 
